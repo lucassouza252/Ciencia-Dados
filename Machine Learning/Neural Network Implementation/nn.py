583e13ca86fdb4d802643b5851cbf3741a71faac
Lucas Souza <lucas252souza@gmail.com>
2021-06-27 01:47:28 -0300

    add backprop

--- Machine Learning/Neural Network Implementation/nn.py
+++ Machine Learning/Neural Network Implementation/nn.py
@@ -98,14 +98,12 @@ class Network(object):
     
     def update_mini_batch(self, mini_batch, eta):
         """
-        
+        Update the biases and weights by each mini_batch with back propagation.
 
         Parameters
         ----------
-        mini_batch : TYPE
-            DESCRIPTION.
-        eta : TYPE
-            DESCRIPTION.
+        mini_batch : Sample to aply back propagantion
+        eta : Learning rate.
 
         Returns
         -------
@@ -123,9 +121,53 @@ class Network(object):
         self.weights = [w-(eta/len(mini_batch)) + nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [d-(eta/len(mini_batch)) + nb for d, nb in zip(self.biases, nabla_b)]
         
+    def backprop(self, x, y):
+        """
         
+
+        Parameters
+        ----------
+        x : TYPE
+            DESCRIPTION.
+        y : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        nabla_b : TYPE
+            DESCRIPTION.
+        nabla_w : TYPE
+            DESCRIPTION.
+
+        """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
         
+        # Feedfoward
+        activation = x
+        # List of activations by layers
+        activations = [x]
+        # List of z by layers
+        zs = []
         
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+            
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l+1].transpose())
+        return (nabla_b, nabla_w)
+    
         
         
         
