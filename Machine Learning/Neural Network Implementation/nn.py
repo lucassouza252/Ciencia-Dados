6fb164a22de30e4ce4cbfc707d63c4d827598b8f
Lucas Souza <lucas252souza@gmail.com>
2021-06-27 01:37:13 -0300

    add new method

--- Machine Learning/Neural Network Implementation/nn.py
+++ Machine Learning/Neural Network Implementation/nn.py
@@ -96,3 +96,45 @@ class Network(object):
             else:
                 print("Epoch {} finished".format(j))
     
+    def update_mini_batch(self, mini_batch, eta):
+        """
+        
+
+        Parameters
+        ----------
+        mini_batch : TYPE
+            DESCRIPTION.
+        eta : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        
+        self.weights = [w-(eta/len(mini_batch)) + nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [d-(eta/len(mini_batch)) + nb for d, nb in zip(self.biases, nabla_b)]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
