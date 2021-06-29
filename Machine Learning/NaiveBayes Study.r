d09c73db4f9b202c50c8a3c37909f45635fb4ada
Lucas Souza <lucas252souza@gmail.com>
2021-06-28 23:04:55 -0300

    add r study

---
+++ Machine Learning/NaiveBayes Study.r
@@ -0,0 +1,21 @@
+install.packages("bnlearn")
+library(bnlearn)
+
+
+install.packages("caret", dependencies=T)
+library(caret)
+
+
+particao = createDataPartition(1:20000,p=.7)
+segurotreino = insurance[particao$Resample1,]
+seguroteste = insurance[- particao$Resample1,]
+dim(segurotreino)
+dim(seguroteste)
+
+modelo = naive.bayes(x=segurotreino,training = "Accident")
+modelo
+plot(modelo)
+
+previsao  = predict(modelo, seguroteste)
+previsao
+confusionMatrix(previsao, seguroteste$Accident)
